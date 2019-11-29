3b35682e22e657b7bbb9c547ed353529461e2d0a
Abhinav Vedati <52864339+Avedati@users.noreply.github.com>
2019-11-29 14:41:20 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,90 +1,96 @@
-/*
-  window.onload()
-  
-  This function is called when the webpage loads.
-*/
 window.onload = function() {
-	/*
-	  summarize()
-	  
-	  This function will summarize a block of text, by removing articles and
-	  discarding less common (in the block of text) words.
-	*/
-	function summarize() {
-		var text = ' ' + document.getElementById('input').value + ' ';
-		var words = text.split(' ').map(function(v) {
-			if(v.charAt(v.length - 1) === '.') { v = v.substring(0, v.length - 1); }
-			return v;
-		});	
-		var prevelance = {};
-		for(var i=0;i<words.length;i++) {
-			if(typeof prevelance[words[i].toLowerCase()] === 'undefined') {
-				prevelance[words[i].toLowerCase()] = 0;
-			}
-			else {
-				prevelance[words[i].toLowerCase()]++;
-			}
-		}
-		var raw_sentences = document.getElementById('input').value.split('.').map(
-			function(v) {
-				return v.split('?').map(
-					function(w) {
-						return w.split('!');
-					}
-				);
-			}
-		);
-		var sentences = [];
-		for(var i=0;i<raw_sentences.length;i++) {
-			for(var j=0;j<raw_sentences[i].length;j++) {
-				for(var k=0;k<raw_sentences[i][j].length;k++) {
-					sentences.push(raw_sentences[i][j][k].trim());
-				}
-			}
-		}
-		var sentences_map = [];
-		var j = 0;
-		sentences.forEach(function(v) {
-			var words = v.split(' ').map(function(w) { return w.trim(); });
-			var totPrev = 0;
-			for(var i=0;i<words.length;i++) { totPrev += prevelance[words[i]] || 0; }
-			sentences_map.push([v, totPrev, j]);
-			j++;
-		});
-		sentences_map.sort(function(a, b) { return b[1] - a[1]; });
-		
-		// var compressionRate = (100 - document.getElementById('compressionRate').value) / 100;
-		// var minPrevelance = sentences_map[Math.ceil((sentences_map.length - 1) * compressionRate)][1];
-
-		var avgPrev = 0;
-		if(sentences_map.length > 0) {
-			for(var i=0;i<sentences_map.length;i++) {
-				avgPrev += sentences_map[i][1];
-			}
-			avgPrev /= sentences_map.length;
-		}
-
-		for(var i=0;i<sentences_map.length;i++) {
-			if(sentences_map[i][1] < avgPrev) {
-				sentences_map.splice(i--, 1);
-			}
-		}
-		sentences_map.sort(function(a, b) { return a[2] - b[2]; });
-		var result = sentences_map.map(function(v) { return v[0]; }).join('. ').trim() + '.';
-		result = result.replace(' are ', ' ');
-		result = result.replace(' is ', ' ');
-		result = result.replace(' a ', ' ');
-		result = result.replace(' an ', ' ');
-		result = result.replace(' the ', ' ');
-		result = result.replace('  ', ' ');
-		document.getElementById('output').innerHTML = result;
+	function getWords(text) {
+		text = text.toLowerCase();
+		text = text.replace(',', '');
+		text = text.replace('.', '');
+		text = text.replace('?', '');
+		text = text.replace('!', '');
+		text = text.replace(/\s[\s]+/, ' ');
+		return text.split(' ');
 	}
 
-	/*
-	  document.getElementById('go').onclick()
-	  
-	  This function is called when the "Summarize" button is clicked.
-	  It will call the summarize function.
-	*/
-	document.getElementById('go').onclick = function() { summarize(); }
-};
+	function getSentences(text) {
+		text = text.replace('.', '.______');
+		text = text.replace('?', '?______');
+		text = text.replace('!', '!______');
+		var result = text.split('______');
+		return result.slice(0, result.length - 1);
+	}
+
+	function summarize() {
+		var sampleParagraph = document.getElementById('input').value;
+		var words = getWords(sampleParagraph);
+		var wordLengths = {};
+		var wordFrequencies = {};
+		for(var i=0;i<words.length;i++) {
+			if(typeof wordLengths[words[i]] === 'undefined') {
+				wordLengths[words[i]] = words[i].length;
+			}
+			if(typeof wordFrequencies[words[i]] === 'undefined') {
+				wordFrequencies[words[i]] = 0
+			}
+			wordFrequencies[words[i]]++;
+		}
+		var wordLengthMax = Math.max.apply(
+			Object.keys(wordLengths).map(function(v) { return wordLengths[v]; })
+		) + 1;
+		var wordFrequencyMax = Math.max.apply(
+			Object.keys(wordFrequencies).map(function(v) { return wordFrequencies[v]; })
+		) + 1;
+		var scores = [];
+		var sentences = getSentences(sampleParagraph);
+		if(wordLengthMax > 0 && wordFrequencyMax > 0) {
+			for(var j=0;j<sentences.length;j++) {
+				var score = 0;
+				var sentence_words = getWords(sentences[j]);
+				for(var i=0;i<sentence_words.length;i++) {
+					if(typeof wordLengths[sentence_words[i]] !== 'undefined' && typeof wordFrequencies[sentence_words[i]] !== 'undefined') {
+						score += wordLengths[sentence_words[i]] / wordLengthMax;
+						score += wordFrequencies[sentence_words[i]] / wordFrequencyMax;
+					}
+				}
+				scores.push(score);
+			}
+		}
+		var avgScore = 0;
+		for(var i=0;i<scores.length;i++) {
+			avgScore += scores[i];
+		}
+		avgScore /= scores.length;
+		for(var i=0;i<scores.length;i++) {
+			if(scores[i] < avgScore) {
+				sentences[i] = '';
+			}
+		}
+		final_sentences = [];
+		for(var i=0;i<sentences.length;i++) {
+			if(sentences[i].length > 0) {
+				final_sentences.push(sentences[i]);
+			}
+		}
+		document.getElementById('output-text').innerHTML = final_sentences.join('.').trim();
+	}
+
+	document.getElementById('test-btn').onclick = function() {
+		if(document.getElementById('explanation').classList.contains('shown')) {
+			document.getElementById('explanation').classList.remove('shown');
+		}
+		document.getElementById('test').classList.add('shown');
+	}
+
+	document.getElementById('expl-btn').onclick = function() {
+		if(document.getElementById('test').classList.contains('shown')) {
+			document.getElementById('test').classList.remove('shown');
+		}
+		document.getElementById('explanation').classList.add('shown');
+	}
+
+	document.getElementById('go').onclick = function() {
+		summarize();
+		document.getElementById('output').classList.add('shown');
+	};
+
+	document.getElementById('stop').onclick = function() {
+		document.getElementById('output').classList.remove('shown');
+	};
+}
